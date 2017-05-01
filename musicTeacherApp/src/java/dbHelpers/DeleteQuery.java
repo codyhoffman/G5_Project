8263f6c35029307eb6a9b90698f601b55f3975ee
Jake Kessel <jake-kessel@uiowa.edu>
2017-05-01 18:20:37 -0500

    added delete functionality

---
+++ musicTeacherApp/src/java/dbHelpers/DeleteQuery.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jake
+ */
+public class DeleteQuery {
+    
+    private Connection conn;
+    
+    public DeleteQuery(){
+        Properties propa= new Properties();
+       InputStream instr=getClass().getResourceAsStream("dbConn.properties");
+        try {
+            propa.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+       String driver= propa.getProperty("driver.name");
+       String url= propa.getProperty("server.name");
+       String username= propa.getProperty("user.name");
+       String passwd= propa.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void doDelete(int studentID){
+        
+        try {
+            //set up a string to hold our query
+            String query = "DELETE FROM students WHERE studentID = ?";
+            //create a preparedstatement using our query string
+            PreparedStatement ps = conn.prepareStatement(query);
+            //fill in the prepared statement
+            ps.setInt(1,studentID);
+            //execute the query
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
